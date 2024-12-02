813c5b9d16593b07375092ad0cd395fd17cf4f73
Axel Lindgren Wenell <atomwar.box@gmail.com>
2024-12-02 20:02:29 +0100

    fix: fixed validator missing messages for certain cases

--- src/middleware/threadMiddleware.js
+++ src/middleware/threadMiddleware.js
@@ -2,20 +2,38 @@ const { body, param } = require("express-validator")
 
 const validateCreateThread = [
     body("author")
+        .notEmpty()
+        .withMessage("Author must be present and can not be omitted from the request.")
         .isMongoId()
         .withMessage("ID must be a valid MongoDB ObjectId."),
-        body("title")
+    body("title")
+        .notEmpty()
+        .withMessage("Title must be present and can not be omitted from the request.")
         .isString()
-        .isLength({ min: 3, max: 30 }),
+        .withMessage("Title must be a string.")
+        .isLength({ min: 3, max: 30 })
+        .withMessage("Title must be between 3 and 30 characters long."),
     body("content")
+        .notEmpty()
+        .withMessage("Content must be present and can not be omitted from the request.")
         .isString()
-        .isLength({ min: 1, max: 1500 }),
+        .withMessage("Content must be a string.")
+        .isLength({ min: 1, max: 1500 })
+        .withMessage("Content must be between 3 and 30 characters long."),
     body("likes")
+        .notEmpty()
+        .withMessage("Likes must be present and can not be omitted from the request.")
         .isNumeric()
-        .isLength({ min: 1 }),
-    body("dislikes")
-        .isNumeric()
+        .withMessage("Likes must be a numeric value.")
         .isLength({ min: 1 })
+        .withMessage("Likes must be at least 1 character long."),
+    body("dislikes")
+        .notEmpty()
+        .withMessage("Dislikes must be present and can not be omitted from the request.")
+        .isNumeric()
+        .withMessage("Disikes must be a numeric value.")
+        .isLength({ min: 1 })
+        .withMessage("Disikes must be at least 1 character long.")
 ]
 
 module.exports = {
--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -3,10 +3,12 @@ const { body, param } = require("express-validator")
 const validateCreateUser = [
     body("username")
         .isAlphanumeric()
+        .withMessage("Username must be an alphanumeric value.")
         .isLength({ min: 3, max: 15 })
         .withMessage("Username must contain between 3 and 15 alphanumerical characters."),
     body("password")
         .isString()
+        .withMessage("Password must be a string.")
         .isLength({ min: 8 })
         .withMessage("Password must be at least 8 characters long.")
 ]
@@ -23,6 +25,7 @@ const validateUpdateUser = [
         .withMessage("ID must be a valid MongoDB ObjectId."),
     body("password")
         .isString()
+        .withMessage("Password must be a string.")
         .isLength({ min: 8 })
         .withMessage("Password must be at least 8 characters long.")
 ]
