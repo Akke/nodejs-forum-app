358395b8bace1b6c2a89fa8ab6843ca9dbd172ac
Axel Lindgren Wenell <atomwar.box@gmail.com>
2024-12-04 03:03:38 +0100

    feat: improved API call efficiency

--- public/js/authCheck.js
+++ public/js/authCheck.js
@@ -1,10 +1,18 @@
+// the cookie is stored in document.cookie so we must extract it
+// contains the JWT token returned by the server after logging in
 function getCookie(name) {
     const value = `; ${document.cookie}`
     const parts = value.split(`; ${name}=`)
     if(parts.length === 2) return parts.pop().split(";").shift()
 }
 
+// when you call this API with a token present, assuming the token is valid,
+// the API returns the user object the token is assosciated with, and then
+// we can store that data in localStorage to later check if the user is logged in 
+// (and also get their data)
 async function setUserData() {
+    if(localStorage.getItem("user")) return // no need to call it if we are already logged in
+
     const jwt = getCookie("jwt")
 
     if(jwt) {
